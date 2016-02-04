a5dcaa0d621cac05277a8b13037f1b8a3abfad78
Jaromír Malenko <jaro@jaro.cz>
2016-02-04 18:02:42 +0100

    Updated Javadoc

--- app/src/main/java/cz/jaro/alarmmorning/WakeLocker.java
+++ app/src/main/java/cz/jaro/alarmmorning/WakeLocker.java
@@ -3,22 +3,37 @@ package cz.jaro.alarmmorning;
 import android.content.Context;
 import android.os.PowerManager;
 
+/**
+ * The WakeLocker prevents the device from sleeping.
+ * <p/>
+ * First, call {@link #acquire(Context)} to prevent the device from sleeping. Second, perform the action. Third, call {@link #release()} to resume the
+ * standard behavior.
+ */
 public abstract class WakeLocker {
     private static final String APP_TAG = "alarm-morning";
 
     private static PowerManager.WakeLock wakeLock;
 
-    public static void acquire(Context ctx) {
+    /**
+     * Prevent the device from sleeping.
+     *
+     * @param context context
+     */
+    public static void acquire(Context context) {
         if (wakeLock != null) wakeLock.release();
 
-        PowerManager pm = (PowerManager) ctx.getSystemService(Context.POWER_SERVICE);
+        PowerManager pm = (PowerManager) context.getSystemService(Context.POWER_SERVICE);
         wakeLock = pm.newWakeLock(PowerManager.FULL_WAKE_LOCK |
                 PowerManager.ACQUIRE_CAUSES_WAKEUP |
                 PowerManager.ON_AFTER_RELEASE, APP_TAG);
         wakeLock.acquire();
     }
 
+    /**
+     * Resume resume the standard behavior regarding sleeping.
+     */
     public static void release() {
-        if (wakeLock != null) wakeLock.release(); wakeLock = null;
+        if (wakeLock != null) wakeLock.release();
+        wakeLock = null;
     }
 }
