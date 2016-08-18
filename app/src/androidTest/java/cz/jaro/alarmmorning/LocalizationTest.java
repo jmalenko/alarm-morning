8d3040d476ffacb1c9566b44a39ce9e15db295d0
jmalenko <ocean123>
2016-08-18 10:43:00 +0200

    Updates tests

--- app/src/androidTest/java/cz/jaro/alarmmorning/LocalizationTest.java
+++ app/src/androidTest/java/cz/jaro/alarmmorning/LocalizationTest.java
@@ -24,28 +24,54 @@ public class LocalizationTest extends AndroidTestCase {
         setLocale("en", "EN");
         Resources res = getContext().getResources();
 
-        assertEquals(Localization.dayOfWeekToString(res, Calendar.SUNDAY), "Sun");
-        assertEquals(Localization.dayOfWeekToString(res, Calendar.MONDAY), "Mon");
-        assertEquals(Localization.dayOfWeekToString(res, Calendar.TUESDAY), "Tue");
-        assertEquals(Localization.dayOfWeekToString(res, Calendar.WEDNESDAY), "Wed");
-        assertEquals(Localization.dayOfWeekToString(res, Calendar.THURSDAY), "Thu");
-        assertEquals(Localization.dayOfWeekToString(res, Calendar.FRIDAY), "Fri");
-        assertEquals(Localization.dayOfWeekToString(res, Calendar.SATURDAY), "Sat");
+        assertEquals("Sunday", Localization.dayOfWeekToString(res, Calendar.SUNDAY));
+        assertEquals("Monday", Localization.dayOfWeekToString(res, Calendar.MONDAY));
+        assertEquals("Tuesday", Localization.dayOfWeekToString(res, Calendar.TUESDAY));
+        assertEquals("Wednesday", Localization.dayOfWeekToString(res, Calendar.WEDNESDAY));
+        assertEquals("Thursday", Localization.dayOfWeekToString(res, Calendar.THURSDAY));
+        assertEquals("Friday", Localization.dayOfWeekToString(res, Calendar.FRIDAY));
+        assertEquals("Saturday", Localization.dayOfWeekToString(res, Calendar.SATURDAY));
+    }
+
+    public void test_dayOfWeekToStringShort_en() {
+        setLocale("en", "EN");
+        Resources res = getContext().getResources();
+
+        assertEquals("Sun", Localization.dayOfWeekToStringShort(res, Calendar.SUNDAY));
+        assertEquals("Mon", Localization.dayOfWeekToStringShort(res, Calendar.MONDAY));
+        assertEquals("Tue", Localization.dayOfWeekToStringShort(res, Calendar.TUESDAY));
+        assertEquals("Wed", Localization.dayOfWeekToStringShort(res, Calendar.WEDNESDAY));
+        assertEquals("Thu", Localization.dayOfWeekToStringShort(res, Calendar.THURSDAY));
+        assertEquals("Fri", Localization.dayOfWeekToStringShort(res, Calendar.FRIDAY));
+        assertEquals("Sat", Localization.dayOfWeekToStringShort(res, Calendar.SATURDAY));
     }
 
     public void test_dayOfWeekToString_cs() {
         setLocale("cs", "CZ");
         Resources res = getContext().getResources();
 
-        assertEquals(Localization.dayOfWeekToString(res, Calendar.SUNDAY), "Ne");
-        assertEquals(Localization.dayOfWeekToString(res, Calendar.MONDAY), "Po");
-        assertEquals(Localization.dayOfWeekToString(res, Calendar.TUESDAY), "Út");
-        assertEquals(Localization.dayOfWeekToString(res, Calendar.WEDNESDAY), "St");
-        assertEquals(Localization.dayOfWeekToString(res, Calendar.THURSDAY), "Čt");
-        assertEquals(Localization.dayOfWeekToString(res, Calendar.FRIDAY), "Pá");
-        assertEquals(Localization.dayOfWeekToString(res, Calendar.SATURDAY), "So");
+        // Czech: záměrně je uveden 4. pád, protože se používá ve větě "Nastavit budík v neděli?"
+        assertEquals("neděli", Localization.dayOfWeekToString(res, Calendar.SUNDAY));
+        assertEquals("pondělí", Localization.dayOfWeekToString(res, Calendar.MONDAY));
+        assertEquals("úterý", Localization.dayOfWeekToString(res, Calendar.TUESDAY));
+        assertEquals("středu", Localization.dayOfWeekToString(res, Calendar.WEDNESDAY));
+        assertEquals("čtvrtek", Localization.dayOfWeekToString(res, Calendar.THURSDAY));
+        assertEquals("pátek", Localization.dayOfWeekToString(res, Calendar.FRIDAY));
+        assertEquals("sobotu", Localization.dayOfWeekToString(res, Calendar.SATURDAY));
     }
 
+    public void test_dayOfWeekToStringShort_cs() {
+        setLocale("cs", "CZ");
+        Resources res = getContext().getResources();
+
+        assertEquals("Ne", Localization.dayOfWeekToStringShort(res, Calendar.SUNDAY));
+        assertEquals("Po", Localization.dayOfWeekToStringShort(res, Calendar.MONDAY));
+        assertEquals("Út", Localization.dayOfWeekToStringShort(res, Calendar.TUESDAY));
+        assertEquals("St", Localization.dayOfWeekToStringShort(res, Calendar.WEDNESDAY));
+        assertEquals("Čt", Localization.dayOfWeekToStringShort(res, Calendar.THURSDAY));
+        assertEquals("Pá", Localization.dayOfWeekToStringShort(res, Calendar.FRIDAY));
+        assertEquals("So", Localization.dayOfWeekToStringShort(res, Calendar.SATURDAY));
+    }
     public void test_dayOfWeekToString_invalid() {
         setLocale("en", "EN");
         Resources res = getContext().getResources();
