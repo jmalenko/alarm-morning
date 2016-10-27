6f08486871b8cb10aac329dcc9f2103ac299a897
jmalenko <ocean123>
2016-10-27 13:24:27 +0200

    Analytics - Wizard

--- app/src/main/java/cz/jaro/alarmmorning/wizard/SetFeaturesSlide.java
+++ app/src/main/java/cz/jaro/alarmmorning/wizard/SetFeaturesSlide.java
@@ -4,6 +4,7 @@ import android.content.SharedPreferences;
 import android.preference.PreferenceManager;
 import android.widget.CheckBox;
 
+import cz.jaro.alarmmorning.Analytics;
 import cz.jaro.alarmmorning.R;
 import cz.jaro.alarmmorning.SettingsActivity;
 
@@ -26,17 +27,36 @@ public class SetFeaturesSlide extends BaseFragment {
         CheckBox onCheckAlarmTime = (CheckBox) getView().findViewById(R.id.onCheckAlarmTime);
         editor.putBoolean(SettingsActivity.PREF_CHECK_ALARM_TIME, onCheckAlarmTime.isChecked());
         if (onCheckAlarmTime.isChecked()) {
-            editor.putString(SettingsActivity.PREF_CHECK_ALARM_TIME_AT, SettingsActivity.PREF_CHECK_ALARM_TIME_AT_DEFAULT);
-            editor.putInt(SettingsActivity.PREF_CHECK_ALARM_TIME_GAP, SettingsActivity.PREF_CHECK_ALARM_TIME_GAP_DEFAULT);
+            savePreference(editor, SettingsActivity.PREF_CHECK_ALARM_TIME_AT, SettingsActivity.PREF_CHECK_ALARM_TIME_AT_DEFAULT);
+            savePreference(editor, SettingsActivity.PREF_CHECK_ALARM_TIME_GAP, SettingsActivity.PREF_CHECK_ALARM_TIME_GAP_DEFAULT);
         }
 
         CheckBox onPlayNighttimeBell = (CheckBox) getView().findViewById(R.id.onPlayNighttimeBell);
         editor.putBoolean(SettingsActivity.PREF_NIGHTTIME_BELL, onPlayNighttimeBell.isChecked());
         if (onPlayNighttimeBell.isChecked()) {
-            editor.putString(SettingsActivity.PREF_NIGHTTIME_BELL_AT, SettingsActivity.PREF_NIGHTTIME_BELL_AT_DEFAULT);
+            savePreference(editor, SettingsActivity.PREF_NIGHTTIME_BELL_AT, SettingsActivity.PREF_NIGHTTIME_BELL_AT_DEFAULT);
         }
 
         editor.commit();
+    }
+
+    private void savePreference(SharedPreferences.Editor editor, String pref, String value) {
+        analytics(pref, value);
+
+        editor.putString(pref, value);
+    }
+
+    private void savePreference(SharedPreferences.Editor editor, String pref, int value) {
+        analytics(pref, value);
+
+        editor.putInt(pref, value);
+    }
+
+    private void analytics(String pref, Object value) {
+        Analytics analytics = new Analytics(getContext(), Analytics.Event.Change_setting, Analytics.Channel.Activity, Analytics.ChannelName.Wizard);
+        analytics.set(Analytics.Param.Preference_key, pref);
+        analytics.set(Analytics.Param.Preference_value, String.valueOf(value));
+        analytics.save();
     }
 
     @Override
