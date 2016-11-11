83d8abdb9f73d549b60e89cd126937cd0f3b3377
jmalenko <ocean123>
2016-11-11 15:47:00 +0100

    Wizard - show warning when run for the 2nd time

--- app/src/main/java/cz/jaro/alarmmorning/wizard/SetWelcomeSlide.java
+++ app/src/main/java/cz/jaro/alarmmorning/wizard/SetWelcomeSlide.java
@@ -1,10 +1,31 @@
 package cz.jaro.alarmmorning.wizard;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
 import cz.jaro.alarmmorning.R;
 
 public class SetWelcomeSlide extends BaseFragment {
 
     private static final String TAG = SetWelcomeSlide.class.getSimpleName();
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = super.onCreateView(inflater, container, savedInstanceState);
+
+        // Set color of bottom description
+        if (Wizard.loadWizardFinished(getContext())) {
+            TextView description2View = (TextView) view.findViewById(R.id.description2);
+            description2View.setTextColor(getResources().getColor(R.color.Deep_Orange_200));
+        }
+
+        return view;
+    }
 
     @Override
     protected String getTitle() {
@@ -12,8 +33,17 @@ public class SetWelcomeSlide extends BaseFragment {
     }
 
     @Override
-    protected String getDescriptionBottom() {
+    protected String getDescriptionTop() {
         return getString(R.string.wizard_welcome_description);
+    }
+
+    @Override
+    protected String getDescriptionBottom() {
+        if (Wizard.loadWizardFinished(getContext())) {
+            return getString(R.string.wizard_welcome_overwrite_warning);
+        } else {
+            return null;
+        }
     }
 
     @Override
