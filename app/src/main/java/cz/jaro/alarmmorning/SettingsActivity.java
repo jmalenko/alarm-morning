1260223df4db777839eacd80c8a9e3b97b80b1e0
Jaromír Malenko <jaro@jaro.cz>
2019-07-11 14:02:43 +0200

    Use plurals in "hour(s) and minute(s)"

--- app/src/main/java/cz/jaro/alarmmorning/SettingsActivity.java
+++ app/src/main/java/cz/jaro/alarmmorning/SettingsActivity.java
@@ -363,8 +363,8 @@ public class SettingsActivity extends AppCompatPreferenceActivity {
                 Context context = preference.getContext();
                 Resources res = context.getResources();
                 String summaryText = hours == 0 ?
-                        res.getQuantityString(R.plurals.pref_summary_snooze_time, minutes, minutes) :
-                        res.getString(R.string.pref_summary_relative_time, hours, minutes);
+                        res.getQuantityString(R.plurals.relative_time_minute, minutes, minutes) :
+                        res.getString(R.string.relative_time_hour_min, res.getQuantityString(R.plurals.relative_time_hour, hours, hours), res.getQuantityString(R.plurals.relative_time_minute, minutes, minutes));
 
                 preference.setSummary(summaryText);
             } else if (preference instanceof TimePreference) {
